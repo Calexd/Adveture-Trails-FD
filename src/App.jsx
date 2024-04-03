8dce2098c55802378565ccb187cb9067ec981d0a
[calexd] <[caledarvs@gmail.com]>
2024-04-03 14:59:05 -0300

    Codigo Base

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import CardTrilha from './CardTrilha.jsx'
+
+function App() {
+
+ 
+
+  return (
+    <>
+      <h1>Card Trilha</h1>
+      <CardTrilha />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/CardTrilha.jsx
@@ -0,0 +1,14 @@
+
+export default function card({subtitulo, titulo = "primero titulo"}){
+    // const [count, setCount] = useState(0)
+  
+    return (
+      <>
+        <h4>{titulo}</h4>
+        <h2>{subtitulo}</h2>
+        <h1>{titulo}</h1>
+      </>
+    )
+  }
+  
+  
